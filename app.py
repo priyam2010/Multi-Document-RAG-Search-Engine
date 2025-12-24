06d84645c7044b68e953e7bb503da1b71a747dfb
Priyam Bhattacharyya <bhattapriyam19@gmail.com>
2025-12-25 01:19:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 import streamlit as st
 from ingestion.loaders import load_pdfs
 from ingestion.chunker import chunk_documents
