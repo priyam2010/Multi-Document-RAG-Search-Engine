afa7cb82695fe0689890df4d0cbfcdfa7950614d
Priyam Bhattacharyya <bhattapriyam19@gmail.com>
2025-12-25 13:44:08 +0530

    Delete chunker.py

--- chunker.py
+++
@@ -1,5 +0,0 @@
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-
-def chunk_documents(documents):
-    splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-    return splitter.split_documents(documents)
