73ac13682860231abb1853c9fd0a1a1bce1e4f0b
Priyam Bhattacharyya <bhattapriyam19@gmail.com>
2025-12-25 13:45:45 +0530

    Delete loaders.py

--- loaders.py
+++
@@ -1,8 +0,0 @@
-from langchain.document_loaders import PyPDFLoader
-
-def load_pdfs(file_paths):
-    docs = []
-    for path in file_paths:
-        loader = PyPDFLoader(path)
-        docs.extend(loader.load())
-    return docs
