0153480bd1fc3f09ce0f2192f31945bd67d698e4
Priyam Bhattacharyya <bhattapriyam19@gmail.com>
2025-12-25 13:46:23 +0530

    Delete web_search.py

--- web_search.py
+++
@@ -1,6 +0,0 @@
-from tavily import TavilyClient
-import os
-
-def tavily_search(query):
-    client = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
-    return client.search(query).get("results", [])
