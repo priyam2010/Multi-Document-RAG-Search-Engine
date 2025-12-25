bb0e5f83e1af670c0826c0c05b03b8cdee124c5c
Priyam Bhattacharyya <bhattapriyam19@gmail.com>
2025-12-25 13:46:03 +0530

    Delete query_router.py

--- query_router.py
+++
@@ -1,7 +0,0 @@
-def route_query(query):
-    q = query.lower()
-    if "latest" in q or "news" in q:
-        return "web"
-    if "document" in q or "paper" in q:
-        return "document"
-    return "hybrid"
