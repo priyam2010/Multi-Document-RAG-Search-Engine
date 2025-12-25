6baf55cd62e7e01fa99c437dcaff19c9473c513f
Priyam Bhattacharyya <bhattapriyam19@gmail.com>
2025-12-25 13:45:28 +0530

    Delete context_builder.py

--- context_builder.py
+++
@@ -1,7 +0,0 @@
-def build_context(doc_chunks, web_results):
-    context = ""
-    for d in doc_chunks:
-        context += d.page_content + "\n"
-    for w in web_results:
-        context += w.get("content", "") + "\n"
-    return context
